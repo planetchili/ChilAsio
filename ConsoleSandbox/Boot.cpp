c596cd25d9f7559ffac845afa5106726cf3c5ec8
Chili <planetchili@users.noreply.github.com>
2024-07-31 10:39:08 +0900

    boot routine in own source

---
+++ ConsoleSandbox/Boot.cpp
@@ -0,0 +1,16 @@
+#include "Boot.h"
+#include <Core/src/log/Log.h>
+#include <Core/src/ioc/Container.h>
+#include <Core/src/log/SeverityLevelPolicy.h>
+#include "CliOptions.h"
+
+using namespace chil;
+
+void Boot()
+{
+	log::Boot();
+
+	ioc::Get().Register<log::ISeverityLevelPolicy>([] {
+		return std::make_shared<log::SeverityLevelPolicy>(*opt::Get().logLevel);
+	});
+}
--- ConsoleSandbox/Main.cpp
+++ ConsoleSandbox/Main.cpp
@@ -1,21 +1,10 @@
 #include <iostream>
 #include <Core/src/log/Log.h>
-#include <Core/src/ioc/Container.h>
-#include <Core/src/log/SeverityLevelPolicy.h>
+#include "Boot.h"
 #include "CliOptions.h"
-
 
 using namespace chil;
 using namespace std::string_literals;
-
-void Boot()
-{
-	log::Boot();
-
-	ioc::Get().Register<log::ISeverityLevelPolicy>([] {
-		return std::make_shared<log::SeverityLevelPolicy>(*opt::Get().logLevel);
-	});
-}
 
 int main()
 {
