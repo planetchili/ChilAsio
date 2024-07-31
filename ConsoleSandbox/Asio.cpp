c52fb36d272f8b4edafc96b83008a17b6b6d33fb
Chili <planetchili@users.noreply.github.com>
2024-07-31 12:17:33 +0900

    connect to server

--- ConsoleSandbox/Asio.cpp
+++ ConsoleSandbox/Asio.cpp
@@ -4,4 +4,5 @@
 #pragma warning(push)
 #pragma warning(disable : 26436 26433 26495 26451 6255 6387 6031 26819 6258 6001 26498 26439 26437)
 #include <boost/asio/impl/src.hpp>
+#include <boost/asio/ssl/impl/src.hpp>
 #pragma warning(pop)
--- ConsoleSandbox/Main.cpp
+++ ConsoleSandbox/Main.cpp
@@ -3,9 +3,14 @@
 #include "Boot.h"
 #include "CliOptions.h"
 #include "Asio.h"
+#include <boost/url.hpp>
+#include <boost/asio/ssl.hpp>
 
 using namespace chil;
 using namespace std::string_literals;
+namespace as = boost::asio;
+namespace url = boost::urls;
+using as::ip::tcp;
 
 int main()
 {
@@ -14,7 +19,33 @@ int main()
 	}
 	Boot();
 
-	chilog.info(L"Test info message");
+	try {
+		url::url url = url::url_view{ "https://www.boots69420.org/LICENSE_1_0.txt" };
+
+		as::io_context ioctx;
+
+		tcp::resolver resolver{ ioctx };
+
+		const std::string server = url.host();
+		const std::string path = url.path();
+
+		auto endpoints = resolver.resolve(tcp::resolver::query{ server, "https" });
+		std::cout << "Resolved url!" << std::endl;
+
+		for (auto& e : endpoints) {
+			std::cout << e.endpoint().address().to_string() << std::endl;
+		}
+
+		as::ssl::context sslContext{ as::ssl::context::sslv23 };
+		as::ssl::stream<tcp::socket> socket{ ioctx, sslContext };
+
+		as::connect(socket.lowest_layer(), endpoints);
+		std::cout << "Connected to server!" << std::endl;
+	}
+	catch (const std::exception& e) {
+		std::cout << typeid(e).name() << ">" << e.what();
+		return -1;
+	}
 
 	return 0;
 }
